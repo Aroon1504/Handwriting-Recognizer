0f3bb3dffe879be10de6195037ab82d03761cf56
Arun <messiarun15042002@gmail.com>
2023-01-16 23:34:22 +0530

    fixed pdf view bug

--- lib/screens/pdf_viewer.dart
+++ lib/screens/pdf_viewer.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
@@ -14,9 +16,11 @@ class _PdfViewerPageState extends State<PdfViewerPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PDFView(
-        filePath: widget.fileModel.path,
-      ),
-    );
+        appBar: AppBar(title: Text(widget.fileModel.name)),
+        body: SfPdfViewer.file(File(widget.fileModel.path))
+        // PDFView(
+        //   filePath: widget.fileModel.path,
+        // ),
+        );
   }
 }
